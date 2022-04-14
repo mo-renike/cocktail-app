02e70ac641360469a99c152ac6af2ccd371a565b
Morenike Oyewole <65955563+mo-renike@users.noreply.github.com>
2022-04-14 16:57:19 +0100

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Cocktails from "./components/Cocktails";
 import { useState, useEffect } from "react";
-import Loader from "./components/Loader";
+// import Loader from "./components/Loader";
 
 function App() {
   const [cocktails, setCocktails] = useState([]);
